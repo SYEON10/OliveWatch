da86716ca6fc3a52550095d5521104146dea9c51
이주연 <37902200+Yongmalyang@users.noreply.github.com>
2025-05-12 18:07:32 +0900

    Feat: Projectile 파일 재생성 (#27)
    * Remove: LFS 제거
    
    * Feat: Projectile 파일 재생성
    
    ---------
    
    Co-authored-by: syeon <syeon.dev@gmail.com>

---
+++ Source/OliveWatch/Public/OWProjectile.h
@@ -0,0 +1,50 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
+#include "OWProjectile.generated.h"
+
+class USphereComponent;
+class UProjectileMovementComponent;
+class UAbilitySystemComponent;
+
+UCLASS(config = Game)
+class OLIVEWATCH_API AOWProjectile : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AOWProjectile();
+
+protected:
+	virtual void BeginPlay() override;
+
+	/** Sphere collision component */
+	UPROPERTY(VisibleDefaultsOnly, Category = Projectile)
+	USphereComponent* CollisionComp;
+
+	/** Projectile movement component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* ProjectileMovement;
+
+	UPROPERTY()
+	FGameplayEffectSpecHandle DamageSpecHandle;
+
+	/** called when projectile hits something */
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+public:
+
+	/** DamageSpec */
+	void SetDamageSpecHandle(FGameplayEffectSpecHandle InHandle) { DamageSpecHandle = InHandle; }
+
+
+	/** Returns CollisionComp subobject **/
+	USphereComponent* GetCollisionComp() const { return CollisionComp; }
+	/** Returns ProjectileMovement subobject **/
+	UProjectileMovementComponent* GetProjectileMovement() const { return ProjectileMovement; }
+};
